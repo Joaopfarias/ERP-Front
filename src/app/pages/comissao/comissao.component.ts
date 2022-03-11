e7b234293ff75b8be24af6876a892f7e852b1595
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-10 23:34:56 -0300

    comissao new front

--- src/app/pages/comissao/comissao.component.ts
+++ src/app/pages/comissao/comissao.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-comissao',
   templateUrl: './comissao.component.html',
-  styleUrls: ['./comissao.component.scss']
+  styleUrls: ['./comissao.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class ComissaoComponent implements OnInit {
   items = [{}, {}, {}]
@@ -26,5 +36,9 @@ export class ComissaoComponent implements OnInit {
       this.click = false;
     }
   }
+
+  funcao(){
+  }
+
 
 }

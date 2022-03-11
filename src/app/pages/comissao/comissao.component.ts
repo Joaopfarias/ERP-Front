02183fd366624a68cd3d887eca989bb7074ce065
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-10 23:40:19 -0300

    fix

--- src/app/pages/comissao/comissao.component.ts
+++ src/app/pages/comissao/comissao.component.ts
@@ -17,28 +17,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 })
 export class ComissaoComponent implements OnInit {
   items = [{}, {}, {}]
-  top10: number = 10
-  top400: number = 400
-  click: boolean = false
-  arrowTo: string = 'down'
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  clickDblClick() {
-    if (this.click == false) {
-      this.click = true;
-      this.arrowTo = 'up'
-    } else {
-      this.arrowTo = 'down'
-      this.click = false;
-    }
-  }
-
-  funcao(){
-  }
-
 
 }

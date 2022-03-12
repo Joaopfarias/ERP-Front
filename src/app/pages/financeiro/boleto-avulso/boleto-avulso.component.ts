8570b11536cbc79d298585132f331ca9b0ed7cbd
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-12 13:35:37 -0300

    financeiro -> boleto-avulso

--- src/app/pages/financeiro/boleto-avulso/boleto-avulso.component.ts
+++ src/app/pages/financeiro/boleto-avulso/boleto-avulso.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./boleto-avulso.component.scss']
 })
 export class BoletoAvulsoComponent implements OnInit {
+  items = [{}, {}, {}]
 
   constructor() { }
 

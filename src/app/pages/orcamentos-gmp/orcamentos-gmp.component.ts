19691abc5978e2d8fd7e6b27b08b1324b9aa8325
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-13 14:43:31 -0300

    orçarmentos-gmp -> cargos

--- src/app/pages/orcamentos-gmp/orcamentos-gmp.component.ts
+++ src/app/pages/orcamentos-gmp/orcamentos-gmp.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-orcamentos-gmp',
   templateUrl: './orcamentos-gmp.component.html',
-  styleUrls: ['./orcamentos-gmp.component.scss']
+  styleUrls: ['./orcamentos-gmp.component.scss'],
 })
 export class OrcamentosGMPComponent implements OnInit {
+  items = [{}, {}, {}]
 
-  constructor() { }
+  isActive: string = 'inactive';
+  tabs = []
+  constructor() {
+  }
+
 
   ngOnInit(): void {
   }
 
+  changeTab(data:boolean){
+    
+  this.tabs = []
+    this.isActive = 'active'
+  }
 }

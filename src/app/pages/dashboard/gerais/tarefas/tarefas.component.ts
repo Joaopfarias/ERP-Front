0faa03e7ff6799dbeec6f5785cffd88973920d00
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-14 16:44:08 -0300

    tarefas

---
+++ src/app/pages/dashboard/gerais/tarefas/tarefas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefas',
+  templateUrl: './tarefas.component.html',
+  styleUrls: ['./tarefas.component.scss']
+})
+export class TarefasComponent implements OnInit {
+  items = [{}, {}, {}]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

f8a17aa38d383ff4e6fa015a7802e82644c11e8b
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-13 23:39:51 -0300

    sistema -> usuarios

--- src/app/pages/dashboard/sistema/usuarios/usuarios.component.ts
+++ src/app/pages/dashboard/sistema/usuarios/usuarios.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.scss']
 })
 export class UsuariosComponent implements OnInit {
+  items = [{}, {}, {}]
 
   constructor() { }
 

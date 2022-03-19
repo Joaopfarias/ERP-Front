84727fdd1ed4a9dae724f55d16cefb8a7aa08c7b
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-19 20:26:10 -0300

    comissao

--- src/app/pages/comissao/configuracao-comissao/configuracao-comissao.component.ts
+++ src/app/pages/comissao/configuracao-comissao/configuracao-comissao.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracao-comissao.component.scss']
 })
 export class ConfiguracaoComissaoComponent implements OnInit {
+  items = [{}, {}, {}]
 
   constructor() { }
 

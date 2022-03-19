1c512a40cc4ebdf0d2749e0b6211f12ec65d820c
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-19 20:08:57 -0300

    dashboard > filial

---
+++ src/app/pages/dashboard/sistema/sindicato/sindicato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sindicato',
+  templateUrl: './sindicato.component.html',
+  styleUrls: ['./sindicato.component.scss']
+})
+export class SindicatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

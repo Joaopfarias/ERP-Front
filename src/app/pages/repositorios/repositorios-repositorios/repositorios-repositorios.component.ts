ad5fbf6d8498e4c5c5905fb7c06966a3f9f63209
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-20 11:35:40 -0300

    repositorios

--- src/app/pages/repositorios/repositorios-repositorios/repositorios-repositorios.component.ts
+++ src/app/pages/repositorios/repositorios-repositorios/repositorios-repositorios.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositorios-repositorios.component.scss']
 })
 export class RepositoriosRepositoriosComponent implements OnInit {
+  items = [{}, {}, {}]
 
   constructor() { }
 
--- src/app/pages/repositorios/tipos/tipos.component.ts
+++ src/app/pages/repositorios/tipos/tipos.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tipos.component.scss']
 })
 export class TiposComponent implements OnInit {
+  items = [{}, {}, {}]
 
   constructor() { }
 

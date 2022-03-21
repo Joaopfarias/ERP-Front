de42d1f2924cbb27fbfdb89e4de641f8b2b5e86a
joaofreire21 <joao.freire@fingerdigital.com.br>
2022-03-20 22:43:49 -0300

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,13 +9,8 @@ import { FuncionariosComponent } from './pages/dashboard/funcionarios/funcionari
 import { GeraisComponent } from './pages/dashboard/gerais/gerais.component';
 import { KeyuserComponent } from './pages/dashboard/keyuser/keyuser.component';
 import { SistemaComponent } from './pages/dashboard/sistema/sistema.component';
-import { ESocialComponent } from './pages/e-social/e-social.component';
 import { FinanceiroComponent } from './pages/financeiro/financeiro.component';
 import { OrcamentosGMPComponent } from './pages/orcamentos-gmp/orcamentos-gmp.component';
-import { ParametrosESocialComponent } from './pages/parametros-e-social/parametros-e-social.component';
-import { ParametrosComponent } from './pages/parametros/parametros.component';
-import { RecursosHumanosComponent } from './pages/recursos-humanos/recursos-humanos.component';
-import { ReinfComponent } from './pages/reinf/reinf.component';
 import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
 import { RepositoriosComponent } from './pages/repositorios/repositorios.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
@@ -28,14 +23,6 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
-  },
-  {
-    path: 'e-social',
-    component: ESocialComponent
-  },
-  {
-    path: 'reinf',
-    component: ReinfComponent
   },
   {
     path: 'repositorios',
@@ -56,19 +43,6 @@ const routes: Routes = [
   {
     path: 'relatorios',
     component: RelatoriosComponent
-  },
-  {
-    path: 'recursos-humanos',
-    
-    component: RecursosHumanosComponent
-  },
-  {
-    path: 'parametros',
-    component: ParametrosComponent
-  },
-  {
-    path: 'parametros-e-social',
-    component: ParametrosESocialComponent
   },
   {
     path: 'header',
